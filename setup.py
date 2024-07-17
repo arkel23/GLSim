9189efc78c45490543ba66cd94101e1a838b8228
ubuntu <calvinku1209@gmail.com>
2024-07-17 18:03:06 +0800

    package description

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup, Command
 
 # Package meta-data.
 NAME = 'glsim'
-DESCRIPTION = 'Global-Local Similarity-Based Anchor Cropping'
+DESCRIPTION = 'Global-Local Similarity for Efficient Fine-Grained Image Recognition with Vision Transformers'
 URL = 'blank'
 EMAIL = 'blank'
 AUTHOR = 'blank'
